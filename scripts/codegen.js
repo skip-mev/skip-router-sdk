1890829f7894e8d4b81b4c13a93c500d28549f78
Griko Nibras <griko@nibras.co>
2024-02-09 03:00:41 +0700

    feat: update telescope codegen

--- scripts/codegen.js
+++ scripts/codegen.js
@@ -23,10 +23,57 @@ async function codegen() {
           useCosmosSDKDec: true,
         },
         enabled: true,
+        exceptions: {
+          "/ethermint.crypto.v1.ethsecp256k1.PrivKey": {
+            aminoType: "ethermint/PrivKeyEthSecp256k1",
+          },
+          "/ethermint.crypto.v1.ethsecp256k1.PubKey": {
+            aminoType: "ethermint/PubKeyEthSecp256k1",
+          },
+        },
         typeUrlToAmino: (typeUrl) => {
+          /**
+           * regex to match typeUrl, e.g.:
+           * - typeUrl  : /ethermint.evm.v1.MsgUpdateParams
+           * - mod      : ethermint
+           * - submod   : evm
+           * - version  : v1
+           * - type     : MsgUpdateParams
+           */
+          const matcher =
+            /^\/(?<mod>\w+)\.(?<submod>\w+)\.(?<version>\w+)\.(?<type>\w+)/;
+
+          const { mod, submod, type } = typeUrl.match(matcher)?.groups ?? {};
+
           // https://github.com/circlefin/noble-cctp/blob/release-2024-01-09T183203/x/cctp/types/codec.go#L30-L56
           if (typeUrl.startsWith("/circle.cctp.v1.Msg")) {
             return typeUrl.replace("/circle.cctp.v1.Msg", "cctp/");
+          }
+
+          /**
+           * lookup mod to assign primary MsgUpdateParams amino type, e.g.:
+           *
+           * - typeUrl  : ethermint.evm.v1.MsgUpdateParams
+           * - mod      : ethermint
+           * - submod   : evm
+           * - type     : ethermint/MsgUpdateParams
+           */
+          const lookup = {
+            ethermint: "evm",
+            evmos: "revenue",
+          };
+
+          if (lookup[mod]) {
+            if (type === "MsgUpdateParams" && lookup[mod] === submod) {
+              return `${mod}/MsgUpdateParams`;
+            }
+            if (type === "MsgUpdateParams") {
+              return `${mod}/${submod}/MsgUpdateParams`;
+            }
+            if (type.startsWith("Msg")) {
+              return `${mod}/${type}`;
+            }
+            return `${submod}/${type}`;
           }
         },
       },
