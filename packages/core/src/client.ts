14cc47d4b898b8702c2955ea925e37f7a773c80c
Griko Nibras <griko@nibras.co>
2024-02-09 03:00:21 +0700

    chore: deregister old ethaccount type

--- packages/core/src/client.ts
+++ packages/core/src/client.ts
@@ -52,7 +52,6 @@ import { AminoConverter as circleAminoConverters } from "./codegen/circle/cctp/v
 import { registry as circleRegistry } from "./codegen/circle/cctp/v1/tx.registry";
 import { erc20ABI } from "./constants/abis";
 import { DEFAULT_GAS_DENOM_OVERRIDES } from "./constants/constants";
-import { EthAccount } from "./ethermint/types";
 import { createTransaction } from "./injective";
 import { RequestClient } from "./request-client";
 import {
@@ -225,7 +224,6 @@ export class SkipRouter {
       ...defaultRegistryTypes,
       ["/cosmwasm.wasm.v1.MsgExecuteContract", MsgExecuteContract],
       ...circleRegistry,
-      [EthAccount.typeUrl, EthAccount],
     ]);
 
     this.endpointOptions = options.endpointOptions ?? {};
