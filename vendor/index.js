45443547d4d302be52b42a13ff23459ad424aa97
wllmshao <wllmshao@gmail.com>
2024-03-29 13:42:47 -0400

    Add proto dependencies

--- vendor/index.js
+++ vendor/index.js
@@ -9,7 +9,9 @@ const protoDirs = [
   path.resolve(__dirname, "evmos/proto"),
   path.resolve(__dirname, "noble-cctp/proto"),
   path.resolve(__dirname, "wasmd/proto"),
-  path.resolve(__dirname, "../local_vendor/initia/proto")
+  path.resolve(__dirname, "../local_vendor/initia/proto"),
+  path.resolve(__dirname, "../local_vendor/cosmos"),
+  path.resolve(__dirname, "../local_vendor/cosmos_proto")
 ];
 
 module.exports = protoDirs;
